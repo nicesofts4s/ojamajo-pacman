123fb2943c57afc38d136f5948f2578b9d125bde
dwth <doctorworsethanhitler@gmail.com>
2018-05-01 02:46:34 -0400

    fixed order of characters
    [s4s]tan should be before dodo since is less expensive

--- src/gameplay/data/characters.h
+++ src/gameplay/data/characters.h
@@ -24,8 +24,8 @@ namespace pacman
                 SHORTS,
                 PACMAN,
                 BURYPINK,
-                DODO,
                 ESFORESTAN,
+                DODO,
 
                 SIZE_CHARACTER
             };
